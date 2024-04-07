d7e388a223e1a57dc7741fa0d50c6b024780d04b
renato <renatochavez7@gmail.com>
2024-04-07 03:13:27 -0300

    deafen command

---
+++ src/commands/deafen.rs
@@ -0,0 +1,41 @@
+use serenity::all::{Context, Message};
+use serenity::all::standard::CommandResult;
+use serenity::all::standard::macros::command;
+use crate::utils::check_msg;
+
+#[command]
+async fn deafen(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild_id = msg.guild(&ctx.cache).unwrap().id;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    let handler_lock = match manager.get(guild_id) {
+        Some(handler) => handler,
+        None => {
+            check_msg(msg.reply(ctx, "Not in a voice channel").await);
+
+            return Ok(());
+        },
+    };
+
+    let mut handler = handler_lock.lock().await;
+
+    if handler.is_deaf() {
+        check_msg(msg.channel_id.say(&ctx.http, "Already deafened").await);
+    } else {
+        if let Err(e) = handler.deafen(true).await {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, format!("Failed: {:?}", e))
+                    .await,
+            );
+        }
+
+        check_msg(msg.channel_id.say(&ctx.http, "Deafened").await);
+    }
+
+    Ok(())
+}
