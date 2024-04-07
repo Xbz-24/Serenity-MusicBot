8c7f76d070391f46841c07b00f00a7eb76fb84a3
renato <renatochavez7@gmail.com>
2024-04-07 03:59:50 -0300

    extract queue

---
+++ src/commands/queue.rs
@@ -0,0 +1,67 @@
+use serenity::all::{Context, Message};
+use serenity::all::standard::{Args, CommandResult};
+use serenity::all::standard::macros::command;
+use songbird::input::YoutubeDl;
+use crate::utils::http_client::get_http_client;
+use crate::utils::utility::check_msg;
+
+#[command]
+#[only_in(guilds)]
+async fn queue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let url = match args.single::<String>() {
+        Ok(url) => url,
+        Err(_) => {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, "Must provide a URL to a video or audio")
+                    .await,
+            );
+
+            return Ok(());
+        },
+    };
+
+    if !url.starts_with("http") {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Must provide a valid URL")
+                .await,
+        );
+
+        return Ok(());
+    }
+
+    let guild_id = msg.guild_id.unwrap();
+
+    let http_client = get_http_client(ctx).await;
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+
+        let src = YoutubeDl::new(http_client, url);
+
+        handler.enqueue_input(src.into()).await;
+
+        check_msg(
+            msg.channel_id
+                .say(
+                    &ctx.http,
+                    format!("Added song to queue: position {}", handler.queue().len()),
+                )
+                .await,
+        );
+    } else {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Not in a voice channel to play in")
+                .await,
+        );
+    }
+
+    Ok(())
+}
