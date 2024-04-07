648db374c78079d8dc6bb8c41e961782122b5c25
renato <renatochavez7@gmail.com>
2024-04-07 03:59:58 -0300

    skip button

---
+++ src/commands/skip.rs
@@ -0,0 +1,38 @@
+use serenity::all::{Context, Message};
+use serenity::all::standard::{Args, CommandResult};
+use serenity::all::standard::macros::command;
+use crate::utils::utility::check_msg;
+
+#[command]
+#[only_in(guilds)]
+async fn skip(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    let guild_id = msg.guild_id.unwrap();
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let handler = handler_lock.lock().await;
+        let queue = handler.queue();
+        let _ = queue.skip();
+
+        check_msg(
+            msg.channel_id
+                .say(
+                    &ctx.http,
+                    format!("Song skipped: {} in queue.", queue.len()),
+                )
+                .await,
+        );
+    } else {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Not in a voice channel to play in")
+                .await,
+        );
+    }
+
+    Ok(())
+}
